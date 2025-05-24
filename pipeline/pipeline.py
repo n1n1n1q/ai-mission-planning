2b50ffd8e1642d2d3323296b00e7ddd78cee3768
BlueSkyAndSomeCurses <65008462+BlueSkyAndSomeCurses@users.noreply.github.com>
2025-05-24 14:25:39 +0300

    2d to 3d

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -3,9 +3,13 @@ import logging
 
 from pathlib import Path
 
-from recon3D.data.video_splitter import video_to_frames
-from recon3D.data.utils import visualize_pcds
-from recon3D.reconstruction.model import load_images, inference, merge_clouds
+# from recon3D.data.video_splitter import video_to_frames
+# from recon3D.data.utils import visualize_pcds
+# from recon3D.reconstruction.model import load_images, inference, merge_clouds
+
+from recon3D.data.io import save_output_dict, load_output_dict
+
+import torch
 
 
 def video_to_point_cloud(video_path):
@@ -19,9 +23,20 @@ def video_to_point_cloud(video_path):
     output_dict = inference(images)
     logging.info("Inference completed")
     pcd = merge_clouds(output_dict)
-    visualize_pcds(pcd)
+
+    return pcd, time_intervals, num_of_frames
 
 
 if __name__ == "__main__":
     video_file = "assets/hackaton videos /IMG_2265.MOV"
-    video_to_point_cloud(video_file)
+    reconstruction_file_path = "data/saved_reconstruction.pkl"
+
+    output_dict = load_output_dict(reconstruction_file_path, torch.device("cpu"))
+    preds_0 = output_dict["preds"][0]
+    print(preds_0.keys())
+    print(preds_0["pts3d_in_other_view"].shape)
+    print(preds_0["pts3d_local"].shape)
+    print(preds_0["conf_local"].shape)
+    print(preds_0["conf"].shape)
+
+    # video_to_point_cloud(video_file)

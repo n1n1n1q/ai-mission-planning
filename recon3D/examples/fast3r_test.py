b470f5bd072bf78a458b958eda76819ef9772dd8
Oleh Basystyi <basystyo@gmail.com>
2025-05-24 13:10:35 +0300

    better storing logic; registration

--- recon3D/examples/fast3r_test.py
+++ recon3D/examples/fast3r_test.py
@@ -10,10 +10,13 @@ from recon3D.data.io import save_output_dict, load_output_dict
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    filepath = "data"
+    filepath = "data/photos"
     images = load_data(filepath)
     logging.info(f"Loaded {len(images)} images from {filepath}")
     output_dict = inference(images)
+    print(output_dict.keys())
+    print(output_dict["views"][0].keys())
+    print(output_dict["preds"][0].keys())
     save_output_dict(output_dict, "saved_reconstruction.pkl")
     logging.info("Inference completed")
     pcd = merge_clouds(output_dict)
